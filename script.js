6be49933882cd9f3ecd9bd872efda56206581339
Giovanni Moreira <giovannimoreira131@gmail.com>
2023-12-01 05:14:36 -0300

    update pagina checklist

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let contador = 0
+
 function clicar_botao(caso) {
     var div = document.getElementById("caso" + caso);
     div.style.backgroundColor = "lightgreen";
+    contador++;
+
+    if (contador == 8) {
+        var botao = document.getElementById("botao_final")
+        botao.style.display = "block"
+    }
+        
+}
+
+function mensagem_final() {
+    var mensagem = document.getElementById("final")
+    mensagem.style.display = "block"
 }
